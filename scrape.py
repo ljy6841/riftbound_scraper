78c1294b288df7231730380ee21e615a88688a70
Tiger Young <ljy6841@rit.edu>
2025-12-21 15:42:59 -0500

    added basic scraping for gallery (only energy cost) and added to requirement

--- scrape.py
+++ scrape.py
@@ -3,9 +3,43 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
+def gallery():
+
+    driver = webdriver.Chrome()
+    URL = "https://riftbound.leagueoflegends.com/en-us/card-gallery/"
+
+    driver.maximize_window()
+    driver.get(URL)
+
+    time.sleep(3)
+    #response = requests.get(URL)
+    html_content = driver.page_source.encode('utf-8').strip()
+    soup = BeautifulSoup(html_content, 'lxml')
+
+    cardLinks = soup.find_all('a', class_="sc-b988531e-0 bvEIZU sc-d043b2-0 bZMlAb")
 
 
-def main():
+    cardLinks = cardLinks[:10]
+
+    for cardLink in cardLinks:
+        link = "https://riftbound.leagueoflegends.com/en-us/card-gallery/" + cardLink.get("href")
+        driver.get(link)
+        time.sleep(0.5)
+        card_html_content = driver.page_source.encode('utf-8').strip()
+        card_content = BeautifulSoup(card_html_content, 'lxml')
+
+        cardInfo = card_content.find('div', class_="sc-60585fca-0 liHgjL")
+        cardCost = cardInfo.div.div.div.p.text
+
+        #print(cardInfo.prettify())
+        print(cardCost)
+        driver.back()
+        time.sleep(0.5)
+
+
+
+
+def piltover():
 
     driver = webdriver.Chrome()
     URL = "https://piltoverarchive.com/decks"
@@ -35,6 +69,11 @@ def main():
 
     #print(decks.prettify())
     #print(deck_name)
+
+def main():
+    gallery()
+
+    
 
 
 if __name__ == "__main__":
